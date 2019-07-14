dea2e147c0f5c1110e96fa0e18554856cb6c5c0e
Marco Loew <marcoloew333@gmail.com>
2019-07-14 21:54:35 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    bets: [],
+    bet: {
+      person_name: "Peter Pan",
+      time_bet: "10:17 Uhr"
+    }
+  };
+
+  componentDidMount() {
+    this.getBets();
+  }
+
+  getBets = _ => {
+    fetch("http://192.168.178.198:4000/products")
+        .then(response => response.json())
+        .then(response => this.setState({bets: response.data}))
+        .catch(err => console.error(err))
+  };
+
+  addBet = _ => {
+    const { bet } = this.state;
+
+    fetch(`http://192.168.178.198:4000/products/add?name=${bet.person_name}&bet=${bet.time_bet}`)
+        .then(response => response.json())
+        .then(this.getProducts)
+        .catch(err => console.error(err))
+  };
+
+  renderBets = ({ bet_id, name}) => <div key={bet_id}>{name}</div>;
+
+    render() {
+    const { bets, bet } = this.state;
+
+        return (
+            <div>
+              {bets.map(this.renderBets)}
+              <div>
+                <input
+                    value={bet.person_name}
+                    onChange={e => this.setState({bet: { ...bet, person_name: e.target.value}})}
+                />
+                <input
+                    value={bet.time_bet}
+                    onChange={e => this.setState({bet: { ...bet, time_bet: e.target.value}})}
+                />
+                <button onClick={this.addBet}>Wette abgeben</button>
+              </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Server/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express(); //initialize express
+
+
+
+const SELECT_ALL_BETS_QUERY = "SELECT * FROM markers";
+
+const connection = mysql.createConnection({
+    host: "www.dev.moodlions.de",
+    user: "d02cda97",
+    password: "*master1819devproj#",
+    database: "d02cda97"
+});
+
+connection.connect(err => {
+    if (err) {
+        return err;
+    }
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("go to /products to see products")
+});
+
+app.get("/products", (req, res) => {
+    connection.query(SELECT_ALL_BETS_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err)
+        }
+        else {
+            return res.json({
+                data: results
+            })
+        }
+    })
+});
+
+app.get("/products/add", (req, res) => {
+    const {name, bet, count1, count2, type} = req.query;
+    // const INSERT_BET = `INSERT INTO markers (id, name, address, lat, lng, type) VALUES ('2, ${name}, ${bet}, ${count1}, ${count2}, ${type}')`;
+    const INSERT_BET = `INSERT INTO markers (name, address, lat, lng, type) VALUES("${name}", "${bet}", 99, 88, "test3")`;
+    connection.query(INSERT_BET, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        else {
+            return res.send("succesfully added your bet")
+        }
+    })
+});
+
+app.listen(4000, () => {
+    console.log("I'm listening to port 4000")
+});
